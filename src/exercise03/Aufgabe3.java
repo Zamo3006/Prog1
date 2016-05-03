43581415976fc58d529c221897d0c58c9186a35e
Budig <1>
2016-05-03 17:31:15 +0200

    dex

--- src/exercise03/Aufgabe3.java
+++ src/exercise03/Aufgabe3.java
@@ -9,31 +9,30 @@ public class Aufgabe3 {
 
 		System.out.println("Zeichen eingeben:");
 		x = scanner.next().charAt(0);
-		int decX = (int) x;
-
+		
 		// Großbuchstabe
-		if (decX >= 'A' && decX <= 'Z') {
+		if (x >= 'A' && x <= 'Z') {
 			System.out.println("Großbuchstabe");
 			// < G
-			if (decX < 'G')
+			if (x < 'G')
 				System.out.println("und Hexadezimale Ziffer");
 		} 
 		
 		// Kleinbuchstabe a-f
-		else if (decX >= 'a' && decX <= 'f') {
+		else if (x >= 'a' && x <= 'f') {
 			System.out.println("Hexadezimale Ziffer");
 		} 
 		
 		// 0-9
-		else if (decX >= '0' && decX <= '9') {
+		else if (x >= '0' && x <= '9') {
 			System.out.println("Hexadezimale Ziffer");
 
 			// < 8
-			if (decX < '8') {
+			if (x < '8') {
 				System.out.println("und Oktale Ziffer");
 				
 				// < 2
-				if (decX < '2') {
+				if (x < '2') {
 					System.out.println("und Binäre Ziffer");
 				}
 				
