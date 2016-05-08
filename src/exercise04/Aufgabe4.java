2176fe52272ee5763ed7d748cde7765c199b5cbc
Budig <1>
2016-05-08 19:26:07 +0200

    Aufgabe4

---
+++ src/exercise04/Aufgabe4.java
@@ -0,0 +1,28 @@
+package exercise04;
+
+import java.util.Scanner;
+
+public class Aufgabe4 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int zahl;
+		boolean prim = true;
+		
+		System.out.println("Biite zu untersuchende Zahl eingeben:");
+		zahl = s.nextInt();
+		
+		for (int i = 2; i < zahl; i++) {
+			if (zahl % i == 0) {
+				prim = false;
+				System.out.println("Untersuchte Zahl ist keine Primzahl!");
+				break;
+			}
+		}
+		
+		if (prim) {
+			System.out.println("Untersuche Zahl ist eine Primzahl!");
+		}
+		
+		s.close();
+	}
+}
