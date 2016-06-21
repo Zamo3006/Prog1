230e7f3856963a7cb25e967714c32bba2b27a5f5
Budig <2>
2016-06-21 14:47:22 +0200

    8 Mini-Änderungen

--- src/exercise08/Aufgabe3.java
+++ src/exercise08/Aufgabe3.java
@@ -1,12 +1,12 @@
 package exercise08;
 
 public class Aufgabe3 {
-public static void main(String[] args) {
-	if(args.length != 2){
-		System.out.println("Bitte genau 2 Zahlen übergeben!");
-		System.exit(1);
+	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Bitte genau 2 Zahlen übergeben!");
+			System.exit(1);
+		}
+		double produkt = Double.valueOf(args[0]) * Double.valueOf(args[1]);
+		System.out.println("Das Produkt beträgt: " + produkt);
 	}
-	double produkt = Double.valueOf(args[0])*Double.valueOf(args[1]);
-	System.out.println("Das Produkt beträgt: " +produkt);
-}
 }
